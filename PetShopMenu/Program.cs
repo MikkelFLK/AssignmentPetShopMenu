86a5e413cef7d66f428b771c6696be84796b57f4
Mikkel K <mikk6578@easv365.dk>
2018-08-29 10:10:23 +0200

    Add project files.

---
+++ PetShopMenu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PetShopMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
