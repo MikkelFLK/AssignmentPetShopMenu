94e6d41637eff39249fc932538a7ffe69ee50dbf
Mikkel K <mikk6578@easv365.dk>
2018-11-20 09:57:19 +0100

    Hope Fix

--- PetShopMenu.Core/ApplicationService/Helpers/JwtSecurityKey.cs
+++ PetShopMenu.Core/ApplicationService/Helpers/JwtSecurityKey.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace PetShopMenu.Core.ApplicationService.Helpers
 {
-    public static class JwtSecurityKey
-    {
-        private static byte[] secretBytes = Encoding.UTF8.GetBytes("A secret for HmacSha256");
 
-        public static SymmetricSecurityKey Key
+        public static class JwtSecurityKey
         {
-            get { return new SymmetricSecurityKey(secretBytes); }
-        }
+            private static byte[] secretBytes = Encoding.UTF8.GetBytes("A secret for HmacSha256");
 
-        public static void SetSecret(string secret)
-        {
-            secretBytes = Encoding.UTF8.GetBytes(secret);
+            public static SymmetricSecurityKey Key
+            {
+                get { return new SymmetricSecurityKey(secretBytes); }
+            }
+
+            public static void SetSecret(string secret)
+            {
+                secretBytes = Encoding.UTF8.GetBytes(secret);
+            }
+
         }
-    }
+    
 }
